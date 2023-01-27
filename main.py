f21d8fa19452adde9171b2d35531432c1fd51ab0
Maxim Romanko <mromanko@list.ru>
2023-01-27 17:57:39 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,121 @@
+from os import listdir
+from os.path import isfile, join
+from typing import List
+
+from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse, FileResponse
+from pathlib import Path
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi import Request, Response
+from fastapi import Header
+from fastapi.templating import Jinja2Templates
+
+CHUNK_SIZE = 1024*1024
+
+
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+video_path = Path("temp/video.mp4")
+rooms = {}
+favicon_path = 'favicon.ico'
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket, room_id: str):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+        rooms[room_id]['users'].append(websocket)
+
+    def disconnect(self, websocket: WebSocket, room_id: str):
+        self.active_connections.remove(websocket)
+        rooms[room_id]['users'].remove(websocket)
+
+    async def broadcast(self, message: str, room_id: str):
+        for connection in rooms[room_id]['users']:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+@app.get("/")
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.htm", context={"request": request})
+
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return FileResponse(favicon_path)
+
+
+@app.get("/video/{id}")
+async def video_endpoint(id, range: str = Header(None)):
+    print(id)
+    video_path = Path("temp/" + id)
+    if range:
+        start, end = range.replace("bytes=", "").split("-")
+        start = int(start)
+        end = int(end) if end else start + CHUNK_SIZE
+    else:
+        start = 0
+        end = start + CHUNK_SIZE
+    with open(video_path, "rb") as video:
+        filesize = str(len(video.read()))
+        video.seek(start)
+        data = video.read(end - start)
+        if end - start > len(data):
+            end = int(filesize)-1
+        headers = {
+            'Content-Range': f'bytes {str(start)}-{str(end)}/{filesize}',
+            'Accept-Ranges': 'bytes'
+        }
+        return Response(data, status_code=206, headers=headers, media_type="video/mp4")
+
+
+@app.post("/enter_room/{id}")
+async def enter_room(id:str):
+    if id not in rooms.keys():
+        rooms[id] = {}
+        rooms[id]['users'] = []
+    response = RedirectResponse(url=f'/room/{id}')
+    return response
+
+
+@app.post("/room/{id}", response_class=HTMLResponse)
+async def join_room(request: Request,id:str):
+    if 'film' in rooms[id].keys():
+        return templates.TemplateResponse("video_playback.htm", context={"request": request, "id": rooms[id]['film']})
+    return templates.TemplateResponse("room.htm", context={"request": request, "id": id})   #TODO return list of videos on server
+
+@app.get("/room/{id}/{film_id}", response_class=HTMLResponse)
+async def join_room(request: Request,id:str, film_id):
+    if id not in rooms.keys():
+        rooms[id] = {}
+        rooms[id]['users'] = []
+    if 'film' not in rooms[id].keys():
+        rooms[id]['film'] = film_id
+        rooms[id]['state'] = 'stopped'
+    print(rooms)
+    return templates.TemplateResponse("video_playback.htm", context={"request": request, "id": rooms[id]['film']})   #TODO return list of videos on server
+
+
+@app.get("/films")
+async def get_all_films():
+    films_list = {}
+    films_list['films'] = [f for f in listdir('temp') if isfile(join('temp', f))]
+    return JSONResponse(content=films_list)                                                             #pick video from list and play it  
+                                                                                            #create ws remote control
+
+@app.websocket("/room/{id}/controls")
+async def control_playback(websocket: WebSocket, id:str):
+    await manager.connect(websocket, id)
+    print(rooms)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast('control', id)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket, id)
+        
